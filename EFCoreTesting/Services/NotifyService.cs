562700a7b8ba70ce574e75de92ec1ae5cc9289ea
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-19 10:45:41 +0300

    + timer

--- EFCoreTesting/Services/NotifyService.cs
+++ EFCoreTesting/Services/NotifyService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace EFCoreTesting.Services
 {
@@ -17,6 +18,26 @@ namespace EFCoreTesting.Services
                 await uvedomlyalka.Invoke(x + 5, y + " Добавка");
             }
             
+        }
+
+        private static Timer timer = new Timer();
+        public void StartTimer()
+        {
+            UpdateTimer();
+        }
+
+        public void UpdateTimer()
+        {
+            
+            timer.AutoReset = true;
+            timer.Interval = 1000;
+            timer.Elapsed += onTimeEvent;
+            timer.Enabled = true;
+        }
+
+        public async  void onTimeEvent(Object source, ElapsedEventArgs e)
+        {
+            await Start(DateTime.Now.Minute, DateTime.Now.ToString());
         }
     }
 }
