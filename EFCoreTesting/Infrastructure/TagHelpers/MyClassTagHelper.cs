73398087c49166a77f1d807596639284face3855
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-26 14:43:47 +0300

    пользовательский тег хелпер

---
+++ EFCoreTesting/Infrastructure/TagHelpers/MyClassTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("myClass", Attributes = "new-class", ParentTag = "div")]
+    public class MyClassTagHelper : TagHelper
+    {
+        public string NewClass { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.SetContent("пользовательский тег хелпер");
+            if (NewClass != null)
+            {
+                output.Attributes.SetAttribute("class", $"bg-{NewClass}");
+            }
+            
+            output.PreContent.SetHtmlContent("<b>");
+            output.PostContent.SetHtmlContent("</b>");
+
+            TagBuilder builder = new TagBuilder("div");
+            builder.InnerHtml.AppendHtml("<p>добавка перед основным содержимым</p>");
+            builder.Attributes.Add("class", "bg-success");
+
+            output.PreElement.AppendHtml(builder);
+        }
+    }
+}
