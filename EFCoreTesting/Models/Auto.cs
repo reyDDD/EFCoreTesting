e9f3d451e0764a910abb3426143df9f3e1cf71b5
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-26 21:34:16 +0300

    DataAnnotationsValidator

---
+++ EFCoreTesting/Models/Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Models
+{
+    public class Auto
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Range(1, 3000, ErrorMessage = "Скорость авто может быть не более 300 км/час")]
+        public decimal MaxSpeed { get; set; }
+
+        [MinLength(3, ErrorMessage ="Название производителя состоит минимум из 3 символов")]
+        public string Brand { get; set; }
+    }
+}
