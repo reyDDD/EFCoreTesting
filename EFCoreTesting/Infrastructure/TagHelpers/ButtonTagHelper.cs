527bb322fe67c4fa687f292e6c570b38115bb03f
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-26 10:44:44 +0300

    Add tag helper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public string ButtonClass { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ButtonClass != null)
+            {
+                output.Attributes.SetAttribute("class", $"btn btn-{ButtonClass}");
+            }
+        }
+    }
+}
