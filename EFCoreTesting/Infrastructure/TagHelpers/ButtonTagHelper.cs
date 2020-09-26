692ff8f95aa8d1594b4b977613744e632a0c172e
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-26 13:08:57 +0300

    pre post content element

--- EFCoreTesting/Infrastructure/TagHelpers/ButtonTagHelper.cs
+++ EFCoreTesting/Infrastructure/TagHelpers/ButtonTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,22 @@ namespace EFCoreTesting.Infrastructure.TagHelpers
         {
             if (ButtonClass != null)
             {
+                output.TagName = "button";
+                output.TagMode = TagMode.StartTagAndEndTag;
                 output.Attributes.SetAttribute("class", $"btn btn-{ButtonClass}");
+                output.Content.SetContent("содержимое тегхелпера");
+                output.Content.AppendHtml(" <b>добавочный текст</b>");
+
+                //для вставки элемента до или после output нужно создать элемент с помощью tagBuilder
+
+                TagBuilder container = new TagBuilder("div");
+                container.Attributes["class"] = "bg-info m-1 p-1";
+                container.InnerHtml.Append("Текст в пре элементе");
+                output.PreElement.SetHtmlContent(container);
+
+                //для вставки контента до или после содержимого output создавать элемент не нужно
+                output.PreContent.SetHtmlContent("<i>");
+                output.PostContent.SetHtmlContent("</i>");
             }
         }
     }
