0db4552b73680591ecf5b2d214359ce61b30aa27
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-27 19:03:45 +0300

    тестирование компонента mvc

---
+++ EFCoreTesting/Components/MyString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Components
+{
+    public class MyString : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Строка в ответе" as object);
+        }
+    }
+}
---
+++ XUnitTestProject1/MyStringComponentTests.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreTesting.Components;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class MyStringComponentTests
+    {
+
+        [Fact]
+        public void TestingMyString()
+        {
+            var myComponent = new MyString();
+
+            var result = myComponent.Invoke() as ViewViewComponentResult;
+
+            Assert.Equal("Строка в ответе", result.ViewData.Model);
+            Assert.IsType<string>(result.ViewData.Model);
+
+        }
+    }
+}
