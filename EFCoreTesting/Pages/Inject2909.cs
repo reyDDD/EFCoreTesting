b92cbae8429ec3123097418ea5dc73d06a436f84
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-29 08:32:02 +0300

    blazor dependecy injection

---
+++ EFCoreTesting/Pages/Inject2909.cs
@@ -0,0 +1,20 @@
+﻿using EFCoreTesting.Models;
+using EFCoreTesting.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Pages
+{
+    public class Inject2909 : ComponentBase
+    {
+
+        [Inject]
+        public Work2809 Work2809 { get; set; }
+
+        [Inject]
+        public Context Context { get; set; }
+    }
+}
