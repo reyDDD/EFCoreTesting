9a395641cf9c8e5a1acb04e97d84bf58dfdfc858
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-29 12:01:38 +0300

    hybrid component

---
+++ EFCoreTesting/Controllers/HybridController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers
+{
+    [ViewComponent(Name = "Hybrid")]
+    public class HybridController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("ogogo" as object);
+        }
+
+        public IViewComponentResult Invoke(string text)
+        {
+            return new ViewViewComponentResult() { ViewData = new ViewDataDictionary<string>(ViewData, text) };
+        }
+    }
+}
