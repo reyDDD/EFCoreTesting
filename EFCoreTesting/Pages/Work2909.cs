14c2d36de24f19fb834576f96d195690f2fdcb2e
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-29 08:53:43 +0300

    blazor partial class

---
+++ EFCoreTesting/Pages/Work2909.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Pages
+{
+    public partial class Work2909
+    {
+        private string result = String.Empty;
+    }
+}
