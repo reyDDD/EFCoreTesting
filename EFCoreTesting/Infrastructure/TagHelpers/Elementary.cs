d46407c3f3019b60e57a37852f5117d922dfcd58
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-09-30 09:36:16 +0300

    user tag helper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/Elementary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("p")]
+    public class Elementary : TagHelper
+    {
+        public string Name { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("namedatut", $"{Name}");
+        }
+    }
+}
