73a864cd50dca6d1250fc6b215af2643b78d48db
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-02 15:29:20 +0300

    Get_EndpointsReturnSuccessAndCorrectContentType

---
+++ IntegrationTests/BasicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<EFCoreTesting.Startup>>
+    {
+        private readonly WebApplicationFactory<EFCoreTesting.Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<EFCoreTesting.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/OneMany/Work2109/")]
+        [InlineData("/")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+
+namespace IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
