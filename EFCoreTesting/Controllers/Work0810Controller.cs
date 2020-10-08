1aa9f9cccad70d443b1a291cf1b7ad2f7fda6b91
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-08 18:53:49 +0300

    Combo0810

---
+++ EFCoreTesting/Controllers/Work0810Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace EFCoreTesting.Controllers
+{
+    [ViewComponent(Name = "Combo0810")]
+    public class Work0810Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<string>(ViewData, "текст для модели")
+            };
+        }
+
+    }
+}
