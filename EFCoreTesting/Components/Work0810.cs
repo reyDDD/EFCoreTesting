4e023c9ef35b1675f4bfa8b307ddce81daadff1a
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-08 18:13:43 +0300

    IViewComponentResult

---
+++ EFCoreTesting/Components/Work0810.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Components
+{
+    public class Work0810 : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string name)
+        {
+             return View("Work0810", name + "прибавка к жалованию");
+        }
+    }
+}
