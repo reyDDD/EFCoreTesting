3d3792d96f14ab490888306a21bbd45fcd495090
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-09 09:59:19 +0300

    MoodTagHelperTests

---
+++ EFCoreTesting/Infrastructure/TagHelpers/MoodTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("boris")]
+    public class MoodTagHelper : TagHelper
+    {
+        public string Moodak { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.Add("class", "pokatit");
+            output.Content.Append(Moodak);
+        }
+    }
+}
---
+++ XUnitTestProject1/MoodTagHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreTesting.Infrastructure.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class MoodTagHelperTests
+    {
+        [Fact]
+        public void TestMood()
+        {
+            var context = new TagHelperContext(new TagHelperAttributeList(), new Dictionary<object, object>(), "uniqueId");
+
+            var output = new TagHelperOutput("div", new TagHelperAttributeList(), (cache, encoder) => 
+            Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+            var mood = new MoodTagHelper() { Moodak = "good" };
+            mood.Process(context, output);
+
+            Assert.Equal("div", output.TagName);
+            Assert.Equal("pokatit", output.Attributes["class"].Value);
+            Assert.Equal(mood.Moodak, output.Content.GetContent());
+        }
+    }
+}
