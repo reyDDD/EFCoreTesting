298142e335ad5eeb9792bbebcd6fb83bafd0c952
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-09 20:01:44 +0300

    TestWithContext

---
+++ XUnitTestProject1/NewContext.cs
@@ -0,0 +1,36 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace XUnitTestProject1
+{
+    public class NewContext : IDisposable
+    {
+        public DbConnection Connection { get; }
+        public NewContext()
+        {
+            Connection = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database=EFCoreTests;Trusted_Connection=True;MultipleActiveResultSets=True;App=EntityFramework;");
+
+            Connection.Open();
+        }
+
+        public Context CreateContext(DbTransaction transaction = null)
+        {
+            var context = new Context(new DbContextOptionsBuilder<Context>().UseSqlServer(Connection).Options);
+            if (transaction != null)
+            {
+                context.Database.UseTransaction(transaction);
+            }
+            return context;
+        }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+        }
+    }
+}
---
+++ XUnitTestProject1/TestWithContext.cs
@@ -0,0 +1,55 @@
+﻿using EFCoreTesting.Controllers;
+using EFCoreTesting.Models;
+using EFCoreTesting.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class TestWithContext : IClassFixture<NewContext>
+    {
+        private NewContext context;
+        public TestWithContext(NewContext context)
+        {
+            this.context = context;
+        }
+
+        [Fact]
+        public void TestWork2809()
+        {
+            using (var transaction = context.Connection.BeginTransaction())
+            {
+                using (var conn = context.CreateContext(transaction))
+                {
+                    Work2809 work2809 = new Work2809(conn);
+                    var controller = new Work0810Controller(work2809);
+
+                    var action = controller.Index2();
+
+                    var result = Assert.IsType<ViewResult>(action);
+                    Assert.IsType<Address>(result.ViewData.Model);
+                }
+
+                using (var conn = context.CreateContext(transaction))
+                {
+                    Work2809 work2809 = new Work2809(conn);
+                    var controller = new Work0810Controller(work2809);
+
+                    var action = controller.Invoke();
+
+                    var result = Assert.IsType<ViewViewComponentResult>(action);
+                    var model = Assert.IsType<string>(result.ViewData.Model);
+                    Assert.Equal("текст для модели", model);
+                }
+
+                transaction.Rollback();
+            }
+
+        }
+    }
+}
