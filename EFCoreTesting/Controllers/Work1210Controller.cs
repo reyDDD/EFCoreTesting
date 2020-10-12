a135d7f46311d40a3831cd280f18730fb9338d1e
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-12 20:12:11 +0300

    ModelFithDataType

--- EFCoreTesting/Controllers/Work1210Controller.cs
+++ EFCoreTesting/Controllers/Work1210Controller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using System.Threading.Tasks;
+using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EFCoreTesting.Controllers
@@ -20,6 +21,13 @@ namespace EFCoreTesting.Controllers
     
             var resu = res.Encode($"<b>некодированный текст с разметкой vot takaya ona</b>");
             return View("Index", resu);
+        }
+
+        public IActionResult DataAnnot()
+        {
+            var res = new ModelFithDataType() {Data1 = DateTime.Now, Data2 = DateTime.Now, Password = "dsd23", EmailAddress = "sd@mail.ru", Currency = "12", PhoneNumber = "0678315004", Time = DateTime.Now, Url = "https://site.ru" };
+
+            return View("DataAnnot", res);
         }
     }
 }
---
+++ EFCoreTesting/Models/ModelFithDataType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Models
+{
+    public class ModelFithDataType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "DataType.Date")]
+        [DataType(DataType.Date)]
+        public DateTime Data1 { get; set; }
+
+        [Display(Name = "DisplayFormat Date")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Data2 { get; set; }
+
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+
+        [DataType(DataType.Currency)]
+        public string Currency { get; set; }
+
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+
+
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+
+        [DataType(DataType.Upload)]
+        public string Upload { get; set; }
+    }
+}
