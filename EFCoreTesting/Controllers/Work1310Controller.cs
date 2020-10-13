204d1221b3ce0475ffb6e9a53e066390ecea8dac
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-13 13:02:43 +0300

    serviceProvider.GetRequiredService<DbContextOptions<Context>>()

---
+++ EFCoreTesting/Controllers/Work1310Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTesting.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers
+{
+    public class Work1310Controller : Controller
+    {
+        private IServiceProvider serviceProvider;
+
+        public Work1310Controller(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public IActionResult Index()
+        {
+            GetContextFromIServiceProvider provider = new GetContextFromIServiceProvider();
+            var context = provider.ReturnContext(serviceProvider);
+            var res = provider.ReturnDataFromBase(context);
+            return View("Address", res.Result);
+        }
+    }
+}
---
+++ EFCoreTesting/Services/GetContextFromIServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Services
+{
+    public class GetContextFromIServiceProvider
+    {
+        public Context ReturnContext(IServiceProvider serviceProvider)
+        {
+            var context = new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>());
+            return context;
+        }
+
+        public async Task<Address> ReturnDataFromBase(Context context)
+        {
+            return await context.Addresses.FirstOrDefaultAsync();
+        }
+
+    }
+}
