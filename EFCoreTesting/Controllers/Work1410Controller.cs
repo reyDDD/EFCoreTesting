6180913755a146224bc002c0d4df147d8b6917d6
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-14 09:28:43 +0300

    return new StatusCodeResult((int)HttpStatusCode.NotFound)

---
+++ EFCoreTesting/Controllers/Work1410Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers
+{
+    public class Work1410Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return new StatusCodeResult((int)HttpStatusCode.NotFound);
+        }
+    }
+}
