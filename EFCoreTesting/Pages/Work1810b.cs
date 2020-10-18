cb44d340b0e4552515fcbfe95e7dc0fe91e105a0
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-18 19:58:51 +0300

    DI partial

---
+++ EFCoreTesting/Pages/Work1810b.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreTesting.Models;
+using EFCoreTesting.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Pages
+{
+    public partial class Work1810b
+    {
+        [Inject]
+        public Work1810Service Servicee { get; set; }
+        public Address Dependas { get; set; }
+
+   
+        public void GetDependency()
+        {
+            Dependas = Servicee.ReturAddressFirst();
+        }
+
+    }
+}
