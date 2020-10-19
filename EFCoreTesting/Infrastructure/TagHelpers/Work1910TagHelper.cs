a90aefed18093403a89e1330f789486a1bf1964f
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-19 18:16:53 +0300

    Work1910TagHelper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/Work1910TagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("chto", Attributes = "propa")]
+    public class Work1910TagHelper : TagHelper
+    {
+
+        public string Propa { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.Attributes.Add("class", $"btn btn-success {Propa}");
+            output.Content.Append("внутренний текст из хелпера");
+        }
+    }
+}
