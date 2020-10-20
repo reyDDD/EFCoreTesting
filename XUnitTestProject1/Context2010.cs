80c2dee216bfb66552388b0f66c87a4eb14473dc
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-20 12:29:03 +0300

    Work2010ApiTests

---
+++ XUnitTestProject1/Context2010.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject1
+{
+    public class Context2010
+    {
+        protected DbContextOptions<Context> ContextOptions { get; }
+        public Context2010(DbContextOptions<Context> contextOptions)
+        {
+            ContextOptions = contextOptions;
+        }
+    }
+}
---
+++ XUnitTestProject1/Work2010ApiTests.cs
@@ -0,0 +1,30 @@
+﻿using EFCoreTesting.Controllers;
+using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class Work2010ApiTests : Context2010
+    {
+        public Work2010ApiTests() : base(new DbContextOptionsBuilder<Context>().UseSqlServer(@"Server = (localdb)\mssqllocaldb; Database=EFCoreTests;Trusted_Connection=True;MultipleActiveResultSets=True;App=EntityFramework;").Options)
+        {
+        }
+
+        [Fact]
+        public async Task ReturnUserFromIdTest()
+        {
+            ApiWork2010Controller controller = new ApiWork2010Controller(new Context(ContextOptions));
+
+            var res = await controller.ReurnUser(22);
+
+            var user = Assert.IsType<ActionResult<User>>(res);
+            Assert.True(user.Value.Id == 22);
+        }
+    }
+}
