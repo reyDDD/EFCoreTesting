03a52bc1656790971f939f2ab5f930cada929f36
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-25 13:45:38 +0200

    Comp2510 : ViewComponent

---
+++ EFCoreTesting/Components/Comp2510.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Components
+{
+    public class Comp2510 : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string naVchod)
+        {
+            return View(naVchod as object);
+        }
+    }
+}
---
+++ EFCoreTesting/Components/Component2510.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FCoreTesting.Components
+{
+    public class Component2510 : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string naVchod)
+        {
+            return View(naVchod as object);
+        }
+    }
+}
