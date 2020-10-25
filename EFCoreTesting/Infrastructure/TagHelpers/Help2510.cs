c7498c2dd9455a6ade5ca30fc123a5e20e0ce65a
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-25 18:29:02 +0200

    simple tag helper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/Help2510.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("li", Attributes = "names")]
+    public class Help2510 : TagHelper
+    {
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.RemoveAll("names");
+            output.Content.Append("пустота внутри списка меню");
+        }
+    }
+
+
+    [HtmlTargetElement("li", Attributes = "named, plus")]
+    public class Help2510b : TagHelper
+    {
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.RemoveAll("named");
+            output.Content.Append("пустота внутри параграфа");
+        }
+    }
+}
