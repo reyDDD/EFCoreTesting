1f76096a2929776b6b30aefa08cd09d3b8598f69
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-25 14:10:36 +0200

    Comp2510Tests

---
+++ XUnitTestProject1/Comp2510Tests.cs
@@ -0,0 +1,22 @@
+﻿using EFCoreTesting.Components;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class Comp2510Tests
+    {
+        [Fact]
+        public void Test2510Component()
+        {
+            Comp2510 compon = new Comp2510();
+
+            ViewViewComponentResult? result = compon.Invoke("eys") as ViewViewComponentResult;
+            Assert.IsType<String>(result.ViewData.Model);
+            Assert.Equal("eys", result.ViewData.Model);
+        }
+    }
+}
