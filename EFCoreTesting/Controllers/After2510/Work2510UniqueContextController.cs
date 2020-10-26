07bfd393ab282c02ca7fdc52f32d79bce7a24712
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-26 07:58:59 +0200

    async Task<IViewComponentResult> InvokeAsync()

--- EFCoreTesting/Controllers/After2510/Work2510UniqueContextController.cs
+++ EFCoreTesting/Controllers/After2510/Work2510UniqueContextController.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace EFCoreTesting.Controllers.After2510
 {
+    [ViewComponent(Name = ("Combinator"))]
     public class Work2510UniqueContextController : Controller
     {
         private Work2510ModelRepo2 modelRepo2;
@@ -14,10 +17,19 @@ namespace EFCoreTesting.Controllers.After2510
         {
             this.modelRepo2 = modelRepo2;
         }
-        public async Task< IActionResult> Index(int id)
+        public async Task<IActionResult> Index(int id)
         {
             var res = await modelRepo2.GetUser(id);
             return View(res);
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<User>(ViewData, await modelRepo2.GetUser(22))
+            };
         }
     }
 }
