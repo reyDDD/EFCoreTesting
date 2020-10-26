1e8f379b845fedf354b644fa14a5bdfe920bebf0
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-26 18:22:49 +0200

    TestDeleteWithIndependentContext

---
+++ IntegrationTests/After2610/TestDeleteWithIndependentContext/BaseInMemoryTests.cs
@@ -0,0 +1,38 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.After2610.TestDeleteWithIndependentContext
+{
+    public class BaseInMemoryTests
+    {
+
+        [Fact]
+        public async void TestDeleteData()
+        {
+            var context = await new ConnToMemory().GetContext();
+
+            List<User> listUser = await context.Users.AsNoTracking().ToListAsync();
+            
+            var user = await context.Users.Where(i=>i.Id == 1).FirstOrDefaultAsync();
+            List<User> listUserWithoutOne = default;
+
+            if (user != null)
+            {
+                listUserWithoutOne = listUser.Where(i => i.Id != user.Id).ToList();
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+            }
+            
+
+            var listAfterRemove = await context.Users.AsNoTracking().ToListAsync();
+
+            Assert.Equal(listUserWithoutOne.OrderBy(i=>i.Id).Select(m=>m.FirstName),
+               listAfterRemove.OrderBy(i => i.Id).Select(m => m.FirstName)) ;
+        }
+    }
+}
---
+++ IntegrationTests/After2610/TestDeleteWithIndependentContext/ConnToMemory.cs
@@ -0,0 +1,48 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.After2610.TestDeleteWithIndependentContext
+{
+    public class ConnToMemory
+    {
+        //первым шагом создаю DbContextOptions 
+        public DbContextOptions<Context> Createoptions()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase("InMemoryDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        //вторым шагом создаю соединение
+        public async Task<Context> GetContext()
+        {
+            Context context = new Context(Createoptions());
+            await Seed(context);
+            return context;
+        }
+
+        //третьим шагом выполняю начальное заполнение базы данных
+        public async Task Seed(Context context)
+        {
+            await context.Addresses.AddAsync(new Address { City = "Киев", Country = "Украина" });
+            await context.Users.AddRangeAsync(new List<User>
+            {
+                new User{FirstName = "Иван", LastName = "Иванов", Age = 22, IsMale = true, BirthDay = DateTime.Now, AddressId = 1},
+                 new User{FirstName = "Петр", LastName = "Петров", Age = 33, IsMale = true, BirthDay = DateTime.Now, AddressId = 1},
+                  new User{FirstName = "Анастасия", LastName = "Анастасьева", Age = 44, IsMale = false, BirthDay = DateTime.Now, AddressId = 1}
+            });
+            await context.SaveChangesAsync();
+        }
+    }
+}
