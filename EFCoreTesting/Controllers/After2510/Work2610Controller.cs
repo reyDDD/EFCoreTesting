d7f37a807e643f22e51de51b25ba985546945390
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-26 08:15:22 +0200

    ResponseCache(Duration = 30, VaryByHeader = "User-Agent"

---
+++ EFCoreTesting/Controllers/After2510/Work2610Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [ResponseCache(Duration = 30, VaryByHeader = "User-Agent", Location = ResponseCacheLocation.Any)]
+    public class Work2610Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("содержимое модели для демонстрации кеша" as object);
+        }
+    }
+}
