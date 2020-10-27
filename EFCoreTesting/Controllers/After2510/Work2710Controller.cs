8935b43ddbb62719002ae25cefbcc0a8c2d953ad
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-27 11:17:55 +0200

    [HttpGet, ActionName("Suda")]

--- EFCoreTesting/Controllers/After2510/Work2710Controller.cs
+++ EFCoreTesting/Controllers/After2510/Work2710Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +18,9 @@ namespace EFCoreTesting.Controllers.After2510
         {
             this.serviceProvider = serviceProvider;
         }
-        public IActionResult Index()
+
+        [HttpGet, ActionName("Suda")]
+        public IActionResult Index(string? name)
         {
             var context = new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>());
             User user = default;
@@ -28,6 +31,30 @@ namespace EFCoreTesting.Controllers.After2510
 
             
             return View(user);
+        }
+
+        [ValidateAntiForgeryToken]
+        public IActionResult Index2([Bind(include: "Id")]User userec)
+        {
+            if (userec.Id == 0)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+            }
+            var context = new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>());
+            User user = default;
+            if (context.Users.Any())
+            {
+                context.Users.Find(userec.Id);
+            }
+
+
+            return View("Index", user);
+        }
+
+
+        public IActionResult Redir(string name, string pustishka)
+        {
+            return CreatedAtAction("Suda", new { name = name} , name); //используется для создания заголовка ответа, фактический вызов метода не происходит. Метод указывает, что в случае вызова данного запроса по пути с такими параметрами будет получен итоговый результатв  третьем параметре
         }
     }
 }
