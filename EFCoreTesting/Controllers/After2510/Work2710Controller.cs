a5127578242c0764937cdc8cbe957a713fb09129
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-27 10:17:43 +0200

    new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>())

---
+++ EFCoreTesting/Controllers/After2510/Work2710Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    public class Work2710Controller : Controller
+    {
+        private IServiceProvider serviceProvider;
+
+        public Work2710Controller(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public IActionResult Index()
+        {
+            var context = new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>());
+            var user = context.Users.FirstOrDefault();
+            return View(user);
+        }
+    }
+}
