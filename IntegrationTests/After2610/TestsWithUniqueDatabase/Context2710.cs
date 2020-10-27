2721078dcf34334d4c1300785409e9f8822fd778
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-27 09:46:42 +0200

    TestWithUniqueContext

---
+++ IntegrationTests/After2610/TestsWithUniqueDatabase/Context2710.cs
@@ -0,0 +1,35 @@
+﻿using EFCoreTesting.Models;
+using IntegrationTests.After2610.TestDeleteWithIndependentContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.After2610.TestsWithUniqueDatabase
+{
+    public class Context2710
+    {
+        private static DbContextOptions<Context> TestDbContextoptions()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase("DataInMemory")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        public  async static Task<Context> ReturnContext()
+        {
+            var context = new Context(TestDbContextoptions());
+
+            await new ConnToMemory().Seed(context);
+            return context;
+        }
+    }
+}
---
+++ IntegrationTests/After2610/TestsWithUniqueDatabase/TestWithUniqueContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.After2610.TestsWithUniqueDatabase
+{
+    public class TestWithUniqueContext
+    {
+        [Fact]
+        public async void TestDemo()
+        {
+            using (var context = await Context2710.ReturnContext())
+            {
+                var res = context.Users.FirstOrDefault();
+
+                Assert.Equal("Иван", res.FirstName);
+            }
+        }
+
+    }
+}
