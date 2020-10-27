fb34338663391613752195838a2d3481a4e4a2a7
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-27 19:27:25 +0200

    Work2710Search

---
+++ EFCoreTesting/Controllers/After2510/Work2710SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    public class Work2710SearchController : Controller
+    {
+        private Context context;
+        public Work2710SearchController(Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index(string searchUser, string country)
+        {
+            ViewBag.Country = new SelectList( await context.Addresses.Select(n=>n.Country).Distinct().ToListAsync());
+
+            var listUser = context.Users.Include(s=>s.Address).AsQueryable();
+            if (searchUser != null)
+            {
+                listUser = listUser.Where(u => u.LastName.Contains(searchUser));
+            }
+            if (country != null)
+            {
+                listUser = listUser.Where(c => c.Address.Country == country);
+            }
+            
+            return View("Index", await listUser.ToListAsync());
+        }
+    }
+}
