ff22af796c0c831aeb8250d890838e2c8ec0f23c
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-28 15:03:48 +0200

    Api2810Controller

---
+++ EFCoreTesting/Controllers/After2510/Api2810Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Api2810Controller : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> ReturnString()
+        {
+            return "znachatina";
+        }
+
+        [HttpGet("{znach:int}")]
+        public async Task<ActionResult<string>> ReturnStringWithParam(int znach)
+        {
+            await Task.FromResult(znach);
+            return znach + ":int";
+        }
+
+        [HttpGet("tut")] //если указать [HttpGet("/tut")], маршрут api/[controller]/tut не отработает, но отработает /tut
+        public async Task<ActionResult<string>> ReturnStringWithParamAndNotQuery(string znach)
+        {
+            await Task.FromResult(znach);
+            return "tut " + znach;
+        }
+    }
+}
