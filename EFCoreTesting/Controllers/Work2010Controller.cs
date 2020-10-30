58a48c357e40595419e9e6f83beb30c81bd63ea1
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-30 19:28:33 +0200

    Настраиваемые атрибуты маршрута

--- EFCoreTesting/Controllers/Work2010Controller.cs
+++ EFCoreTesting/Controllers/Work2010Controller.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace EFCoreTesting.Controllers
 {
+
+    public class YaPlakalKogdaSmotrel : Attribute, IRouteTemplateProvider
+    {
+        public string Name => "estTakoy";
+
+        public int? Order => 2;
+
+        public string Template => "otkudaOnVsyalsya";
+    }
+
+    [YaPlakalKogdaSmotrel]
     public class Work2010Controller : Controller
     {
         public IActionResult Index()
