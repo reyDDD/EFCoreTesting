2664500bb06f2ad93a1e4f227c5c89c432de65cd
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-10-31 17:51:49 +0200

    Configuration.GetSection("TestSection").Get<Uzver>()

--- EFCoreTesting/Areas/Two/Controllers/HomeTwoController.cs
+++ EFCoreTesting/Areas/Two/Controllers/HomeTwoController.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Core.Configuration;
 using EFCoreTesting.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace EFCoreTesting.Areas.Two.Controllers
 {
+
+    public class Uzver
+    {
+        public readonly string nameConf = "Юзверь";
+        public string User { get; set; }
+        public string Age { get; set; }
+    }
+
+
     [Area("Two")]
     public class HomeTwoController : Controller
     {
+        private readonly IConfiguration Configuration;
+        public HomeTwoController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public IActionResult Index([FromServices] IService3110 service)
         {
             return View("Index", service.ReturnName());
         }
+
+        public IActionResult Index2()
+        {
+            var user = Configuration.GetSection("TestSection").Get<Uzver>();
+            return View("Index", user.User);
+        }
+
     }
 }
