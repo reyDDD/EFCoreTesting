8b00bf26500347b9dbbbb33e6bf36f41a1559f39
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-02 11:32:42 +0200

    ApiController

---
+++ EFCoreTesting/Controllers/After2510/Work0211.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class Work0211 : ControllerBase
+    {
+        public IActionResult ReturnString(string first)
+        {
+            return Ok(first + " success");
+        }
+    }
+}
