6827b270c9df14768f93121b50b2e87dff9e70bf
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-03 14:53:22 +0200

    TestFactDeleteData

--- XUnitTestProject1/After2610/Work0311Tests.cs
+++ XUnitTestProject1/After2610/Work0311Tests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace XUnitTestProject1.After2610
 {
@@ -18,6 +19,24 @@ namespace XUnitTestProject1.After2610
             await conn.SaveChangesAsync();
             var count = await conn.Addresses.CountAsync();
             Assert.True(count > 0);
+        }
+
+        [Fact]
+        public async void TestFactDeleteData()
+        {
+            var conn = new Context(UniqueContextOptionsForTesting.OptionsForContext());
+
+            var addr = conn.Addresses.Add(new Address { City = "Gorod", Country = "Strana" });
+            await conn.SaveChangesAsync();
+
+            var expectedAddresses = conn.Addresses.Where(a => a.Id != addr.Entity.Id);
+            var yes = conn.Addresses.Find(1);
+            conn.Remove(yes);
+            await conn.SaveChangesAsync();
+
+            var actualAddress = await conn.Addresses.AsNoTracking().ToListAsync();
+            Assert.Equal(expectedAddresses?.OrderBy(m => m.Id)?.Select(i=>i.Id),
+                actualAddress?.OrderBy(m => m.Id)?.Select(i => i.Id));
         }
     }
 }
