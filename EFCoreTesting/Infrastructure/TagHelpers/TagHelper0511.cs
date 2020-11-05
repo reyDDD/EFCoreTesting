dce9690596b167a7db2d96610a835c46d1999d06
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-05 13:29:12 +0200

    new TagHelper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/TagHelper0511.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("li", Attributes = "nachalo*")]
+    public class TagHelper0511 : TagHelper
+    {
+        public string NachaloKonca { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.Append(NachaloKonca);
+            output.Attributes.Add("class", "danger");
+        }
+    }
+}
