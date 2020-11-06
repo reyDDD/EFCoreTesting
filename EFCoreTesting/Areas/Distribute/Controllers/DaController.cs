0318e8812b592e7124c6c2661619c4f879f44681
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-06 15:10:53 +0200

    config.GetConnectionString("DefaultConnection");

--- EFCoreTesting/Areas/Distribute/Controllers/DaController.cs
+++ EFCoreTesting/Areas/Distribute/Controllers/DaController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCoreTesting.Areas.Distribute.Controllers
 {
     [Area("Distribute")]
     public class DaController : Controller
     {
+
+        private IConfiguration config;
+
+        public DaController()
+        {
+            config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build(); //IConfigurationRoot реализует интерфейс iConfiguration
+        }
         public IActionResult Index()
         {
+            string stroca = config.GetConnectionString("DefaultConnection");
             return View("Index", new TwoData { Data1 = DateTime.Now, Data2 = DateTime.Now });
         }
 
