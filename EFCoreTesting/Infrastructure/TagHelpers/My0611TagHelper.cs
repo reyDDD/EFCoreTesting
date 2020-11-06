09482a65363112c59d3e0b5754b755dc1c5aff62
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-06 11:28:07 +0200

    My0611TagHelper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/My0611TagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("put", Attributes= "yestTakoy", ParentTag = "div", TagStructure = TagStructure.WithoutEndTag)]
+    public class My0611TagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.AppendHtml("<em>здесь был Вася</em>");
+
+        }
+    }
+}
