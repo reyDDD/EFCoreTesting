2e1f5016bb1bc95512dd2e788154038260d7fc72
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-07 19:03:43 +0200

    getAddressTo() {

---
+++ EFCoreTesting/wwwroot/js/crud0711.js
@@ -0,0 +1,15 @@
+﻿window.onload = ()=>{
+    //да нихуя оно не работает
+ 
+}
+
+async function getAddressTo() {
+    let id = document.getElementById("numberAddr");
+    let url = "https://localhost:44356/api/Api0711/ReturnDto/dto/22";
+    var request = await fetch(url);
+    let response = await request.json();
+
+    var kuda = document.getElementById("insertgettingAddress");
+    kuda.innerHTML = "<em>" + response.city + "</em>";
+    alert(response.city);
+}
