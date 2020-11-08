1f098799720de497e9014ca6ff94f8b23149c904
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-08 21:08:32 +0200

    DeleteNumberParameterTransformer

---
+++ EFCoreTesting/Infrastructure/DeleteNumberParameterTransformer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure
+{
+    public class DeleteNumberParameterTransformer : IOutboundParameterTransformer
+    {
+        public string TransformOutbound(object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            return Regex.Replace(value.ToString(), "[0-9]+", "");
+        }
+    }
+}
---
+++ XUnitTestProject1/After2610/DeleteNumberParameterTransformerTest.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreTesting.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1.After2610
+{
+    public class DeleteNumberParameterTransformerTest
+    {
+
+        [Fact]
+        public void Replace()
+        {
+            DeleteNumberParameterTransformer transformer = new DeleteNumberParameterTransformer();
+
+            var output = transformer.TransformOutbound("Work1209ss");
+            Assert.Equal("Workss", output);
+        }
+    }
+}
