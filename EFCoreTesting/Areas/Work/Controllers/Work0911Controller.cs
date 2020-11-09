797b0d5fa9fe237c57000ddb5207cd100b654e23
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-09 15:24:39 +0200

    configuration.GetSection - two way

--- EFCoreTesting/Areas/Work/Controllers/Work0911Controller.cs
+++ EFCoreTesting/Areas/Work/Controllers/Work0911Controller.cs
@@ -5,17 +5,44 @@ using System.Threading.Tasks;
 using EFCoreTesting.Models;
 using EFCoreTesting.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCoreTesting.Areas.Work.Controllers
 {
+
+    public class Uzzer
+    {
+        public string Name { get; set; } = "TestSection3";
+        public string User { get; set; }
+        public string Age { get; set; }
+    }
     [Area("Work")]
     public class Work0911Controller : Controller
     {
+        private IConfiguration configuration;
+        public Work0911Controller(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public async Task<ActionResult<string>> Index([FromServices] IVozvrat2909 vozvrat)
         {
             var addr = await vozvrat.GetAddress();
             var spisok = addr.ToList().FirstOrDefault();
             return View("Index", "with area+" + " " + spisok.City);
+        }
+
+        public async Task <ActionResult> GetDataFromConfigaration()
+        {
+            Uzzer user = new Uzzer();
+            configuration.GetSection(user.Name).Bind(user);
+            return View("Index", user.User + " " + user.Age);
+        }
+
+        public async Task<ActionResult> GetDataFromConfigaration2()
+        {
+            Uzzer user = new Uzzer();
+            Uzzer result = configuration.GetSection(user.Name).Get<Uzzer>();
+            return View("Index", result.User + " " + result.Age);
         }
     }
 }
