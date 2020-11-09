d67c8188649ec3c3b7b30f067172ffebfab077e6
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-09 12:46:54 +0200

    Api0911Controller

---
+++ EFCoreTesting/Controllers/After2510/Api0911Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Api0911Controller : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public ActionResult<string> Index(string name)
+        {
+            return name;
+        }
+    }
+}
--- EFCoreTesting/Controllers/After2510/Work0811Controller.cs
+++ EFCoreTesting/Controllers/After2510/Work0811Controller.cs
@@ -14,7 +14,14 @@ namespace EFCoreTesting.Controllers.After2510
         //[HttpGet("{name}")]
         public IActionResult Index(int name)
         {
-            return View("dd" as object);
+            double meal_cost = 15.50;
+
+            int tip_percent = 15;
+
+            int tax_percent = 10;
+
+            int result = Convert.ToInt32(Math.Round(meal_cost / 100 * tip_percent + meal_cost / 100 * tax_percent + meal_cost));
+            return View(result as object);
         }
     }
 }
