fc182ae2e3f838e384860e3d325dfeb3f9a6f5a8
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-09 12:56:42 +0200

    Api0911Attribute

---
+++ EFCoreTesting/Controllers/After2510/Api0911bController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Api0911]
+    [ApiController]
+    public class Api0911bController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index(string pipi)
+        {
+
+            return pipi + " pipi";
+        }
+    }
+}
---
+++ EFCoreTesting/Controllers/Api0911Attribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers
+{
+    public class Api0911Attribute : Attribute, IRouteTemplateProvider
+    {
+        public string Name { get; set; }
+
+        public int? Order => 2;
+
+        public string Template => "pipi/[controller]";
+    }
+}
