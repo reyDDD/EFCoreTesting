622d7de5dc51b105f7df0123461826b0ae7d63eb
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-10 19:11:15 +0200

    [FromServices] IMemoryCache memoryCache

--- EFCoreTesting/Areas/Distribute/Controllers/Work1011Controller.cs
+++ EFCoreTesting/Areas/Distribute/Controllers/Work1011Controller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace EFCoreTesting.Areas.Distribute.Controllers
 {
@@ -24,10 +25,33 @@ namespace EFCoreTesting.Areas.Distribute.Controllers
             return View();
         }
 
+       
         public IActionResult IndexWithNewContext([FromServices] Context1011 context1011)
         {
             string city = context1011.Addresses.FirstOrDefault().City;
             return View("Index", city);
         }
+
+
+        [ResponseCache(Duration = 40)]
+        //[ResponseCache(VaryByQueryKeys = new string[] { "param", "dd", "bb" }, Duration = 30)] //если один из ключей явяляется параметром метода и его значение меняется, тогда программа должна возвращать новый результат, иначе - кешированный. По факту не работает!!!
+        public IActionResult TestVaryByQueryKeys(string param)
+        {
+            string data = DateTime.Now.TimeOfDay.ToString(); 
+            return View("Index", data);
+        }
+
+        public IActionResult TestIMemoryCache([FromServices] IMemoryCache memoryCache)
+        {
+            DateTime dates;
+            if (!memoryCache.TryGetValue("key", out dates))
+            {
+                dates = DateTime.Now;
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(10));
+                memoryCache.Set("key", dates, cacheEntryOptions);
+            }
+            return View("Index", dates.TimeOfDay.ToString());
+        }
+
     }
 }
