cb6f5203da9ae06a0e566566684e6d7136b683ef
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-13 16:23:14 +0200

    scope ilogger

--- EFCoreTesting/Areas/Distribute/Controllers/Work1311Controller.cs
+++ EFCoreTesting/Areas/Distribute/Controllers/Work1311Controller.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Alachisoft.NCache.Common.Logger;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreTesting.Areas.Distribute.Controllers
 {
@@ -11,17 +13,30 @@ namespace EFCoreTesting.Areas.Distribute.Controllers
     public class Work1311Controller : Controller
     {
         private IConfiguration configuration;
+        private ILogger<Work1311Controller> logger;
 
-        public Work1311Controller(IConfiguration configuration)
+        public Work1311Controller(IConfiguration configuration, ILogger<Work1311Controller> logger)
         {
             this.configuration = configuration;
+            this.logger = logger;
         }
 
         
         public IActionResult Index()
         {
-            string res = configuration.GetValue<string>("первый");
+            string res;
+            using (logger.BeginScope("start debug scoupe"))
+            {
+                logger.LogDebug("Read this on Saturday");
+                res = configuration.GetValue<string>("первый");
+            }
             return View("Index", res);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            logger = null;
+            base.Dispose(disposing);
         }
     }
 }
