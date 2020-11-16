0cd7f19e3f5ff1d0ea951e2720ef69fd2d67a871
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-16 14:56:59 +0200

    [Work1611Attribute]

---
+++ EFCoreTesting/Controllers/After2510/Work1611bController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTesting.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Work1611Attribute]
+    public class Work1611bController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("This have worked" as object);
+        }
+    }
+}
---
+++ EFCoreTesting/Infrastructure/Work1611Attribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure
+{
+    public class Work1611Attribute : Attribute, IRouteTemplateProvider
+    {
+        public string Name => "Work1611Case";
+
+        public int? Order => 2;
+
+        public string Template => "tudaSuda/[controller]/[action]";
+    }
+}
