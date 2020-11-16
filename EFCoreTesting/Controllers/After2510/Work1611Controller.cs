5561314bc1cb887ddc88b0b5e38a44848ad26ccb
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-16 11:41:30 +0200

    [HttpGet("{age}")] - без параеметра маршрут не будет существоать

---
+++ EFCoreTesting/Controllers/After2510/Work1611Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    public class Work1611Controller : Controller
+    {
+        [HttpGet("{age}")]
+        public IActionResult Index(int age)
+        {
+            return View(age);
+        }
+    }
+}
---
+++ XUnitTestProject1/Controllers/After2510/Work1611.cs
@@ -0,0 +1,26 @@
+﻿using EFCoreTesting.Controllers.After2510;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1.Controllers.After2510
+{
+    public class Work1611
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(0)]
+        public void RequiredParameter(int parameter)
+        {
+            Work1611Controller work = new Work1611Controller();
+            var actionResult = work.Index(parameter);
+
+            var viewresult = Assert.IsType<ViewResult>(actionResult);
+            Assert.IsType<int>(viewresult.ViewData.Model);
+            Assert.Equal(parameter, viewresult.ViewData.Model);
+
+        }
+    }
+}
