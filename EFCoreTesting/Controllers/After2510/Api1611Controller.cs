4b2c4fab00f49ab98ba9f871ebd78c82a8960f9b
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-16 11:19:23 +0200

    [Route("api/[controller]/[action]")]

---
+++ EFCoreTesting/Controllers/After2510/Api1611Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Route("api/[controller]/[action]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class Api1611Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Work()
+        { 
+            return Ok("otwet");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult WorkId(int id)
+        {
+            return Ok("otwet: " + id);
+        }
+    }
+}
