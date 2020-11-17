46e8343906ec75a712e41823fb5dfeed439636f8
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-17 12:50:45 +0200

    logger.BeginScope

--- EFCoreTesting/Controllers/After2510/Work1711Controller.cs
+++ EFCoreTesting/Controllers/After2510/Work1711Controller.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Core.Logging;
 using EFCoreTesting.Infrastructure.DIWithParam;
 using EFCoreTesting.Infrastructure.Mediator1711;
 using EFCoreTesting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreTesting.Controllers.After2510
 {
@@ -14,13 +16,14 @@ namespace EFCoreTesting.Controllers.After2510
         private Context context;
         private IObbrabotchik<StartClass, EndClass> obbrabotchik;
         IMedi<StartClass> medi;
+        private ILogger<Work1711Controller> logger;
 
-
-        public Work1711Controller(Context context, IObbrabotchik<StartClass, EndClass> obbrabotchik, IMedi<StartClass> medi)
+        public Work1711Controller(Context context, IObbrabotchik<StartClass, EndClass> obbrabotchik, IMedi<StartClass> medi, ILogger<Work1711Controller> logger)
         {
             this.context = context;
             this.obbrabotchik = obbrabotchik;
             this.medi = medi;
+            this.logger = logger;
         }
         public IActionResult Index()
         {
@@ -56,6 +59,18 @@ namespace EFCoreTesting.Controllers.After2510
 
         public IActionResult Text()
         {
+            logger.BeginScope("start my scoped");
+        
+                logger.LogError("first");
+                logger.LogError("second");
+        
+            using (logger.BeginScope("start my seong scoped"))
+            {
+                logger.LogError("first in second");
+                logger.LogError("second in second");
+            }
+
+
             return View("Index");
         }
     }
