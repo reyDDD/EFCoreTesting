c5b3d653d5fcf947934500c407f53c2e9cab6baa
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-17 10:33:29 +0200

    Select(m => new Moda { City = m.City, Country = m.Country })

---
+++ EFCoreTesting/Controllers/After2510/Work1711Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    public class Work1711Controller : Controller
+    {
+        private Context context;
+
+        public Work1711Controller(Context context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            Moda model = context.Addresses.Where(i => i.City != null).Select(m => new Moda { City = m.City, Country = m.Country }).FirstOrDefault();
+
+            return Ok($"{model.City} {model.Country}");
+        }
+
+        public class Moda
+        {
+            public string City { get; set; }
+            public string Country { get; set; }
+        }
+    }
+}
