45670e959fd5331c27cf183e0f50977f7be17807
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-21 16:25:29 +0200

    загрузка связанных коллекций

---
+++ EFCoreTesting/Models/Zaprosy2111.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Models
+{
+    public class Zaprosy2111
+    {
+        private Context context;
+        public Zaprosy2111(Context context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Address> ReturnTwoCollections()
+        {
+            var adr = context.Addresses.ToList();
+
+            foreach (var item in adr)
+            {
+                context.Entry(item).Collection(u => u.Users).Query().Where(x => x.IsMale == true).Load();
+            }
+            return adr;
+        }
+    }
+}
