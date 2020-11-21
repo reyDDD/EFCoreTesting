a4a9a4027a5aa0f88122d6bc97bcac4608c888a2
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-21 14:38:49 +0200

    partial class razor

---
+++ EFCoreTesting/Pages/After2111/PageNull.razor.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreTesting.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Pages.After2111
+{
+    public partial class PageNull
+    {
+        //[Parameter]
+        public string Data { get; set; }
+
+        [Inject]
+        Vozvrat2909 repo { get; set; }
+
+        private async Task Work()
+        {
+            var res = await repo.GetAddress();
+            Data = res.ToList().FirstOrDefault().City;
+        }
+    }
+}
