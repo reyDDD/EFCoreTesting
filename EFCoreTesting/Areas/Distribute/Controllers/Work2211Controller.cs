9ea3fcdbc36049f4aad805a19953ae2e7b83c34f
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-22 12:39:36 +0200

    TryUpdateModelAsync<User>()

--- EFCoreTesting/Areas/Distribute/Controllers/Work2211Controller.cs
+++ EFCoreTesting/Areas/Distribute/Controllers/Work2211Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,19 @@ namespace EFCoreTesting.Areas.Distribute.Controllers
     public class Work2211Controller : Controller
     {
         [ActionName("Secret")]
-        public IActionResult Index(int id)
+        public async Task< IActionResult> Index(int id, User user, [FromServices] Context context)
         {
             if (id is 0 )
             {
                 return new StatusCodeResult((int)HttpStatusCode.Found);
+            }
+            else if(id is > 0)
+            {
+                User userr = context.Users.Single(x => x.Id == id);
+                if (await TryUpdateModelAsync<User>(userr, "", x => x.IsMale, x=>x.FirstName))
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.OK);
+                }
             }
             return View();
         }
