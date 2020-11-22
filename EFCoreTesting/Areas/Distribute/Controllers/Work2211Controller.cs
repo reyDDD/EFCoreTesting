a0c51dca5ca1def8f9df4b69b120ce85d65a17c1
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-22 12:29:07 +0200

    [ActionName("Secret")]

--- EFCoreTesting/Areas/Distribute/Controllers/Work2211Controller.cs
+++ EFCoreTesting/Areas/Distribute/Controllers/Work2211Controller.cs
@@ -10,6 +10,7 @@ namespace EFCoreTesting.Areas.Distribute.Controllers
     [Area("Distribute")]
     public class Work2211Controller : Controller
     {
+        [ActionName("Secret")]
         public IActionResult Index(int id)
         {
             if (id is 0 )
