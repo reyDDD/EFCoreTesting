b2a9ef2ea3cfc1f65fd7d374daa5609e4d6b3b46
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-22 12:27:18 +0200

    new StatusCodeResult((int)HttpStatusCode.Found)

---
+++ EFCoreTesting/Areas/Distribute/Controllers/Work2211Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Areas.Distribute.Controllers
+{
+    [Area("Distribute")]
+    public class Work2211Controller : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            if (id is 0 )
+            {
+                return new StatusCodeResult((int)HttpStatusCode.Found);
+            }
+            return View();
+        }
+    }
+}
