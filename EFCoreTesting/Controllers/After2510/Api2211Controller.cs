64fb79c7b32a9a9be3431de8742b6fa9db6e1883
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-22 16:47:41 +0200

    return CreatedAtAction("Index", new { userId = user.Id }, user)

---
+++ EFCoreTesting/Controllers/After2510/Api2211Controller.cs
@@ -0,0 +1,36 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2510
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Api2211Controller : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public IActionResult Add([FromBody]User user, [FromServices]Context context)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+            return CreatedAtAction("Index", new { userId = user.Id }, user);
+        }
+
+        [Produces("application/json")]
+        [HttpGet("{userId}")]
+        public IActionResult Index(long userId, [FromServices] Context context)
+        {
+            var user = context.Users.Include(c => c.Address).Where(x => x.Id == userId).FirstOrDefault();
+
+            return Ok(user);
+        }
+    }
+}
