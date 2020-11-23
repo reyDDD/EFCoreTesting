ff350677e9516efd229ceca1fb5f4f766eb5852b
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-23 20:41:47 +0200

    distributed-cache tag helper

---
+++ EFCoreTesting/Areas/Distribute/Controllers/Work2311Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Areas.Distribute.Controllers
+{
+    [Area("Distribute")]
+    public class Work2311Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
