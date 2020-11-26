abe5df1613b1309a9475224547490e7cc408da96
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-26 18:54:23 +0200

    testing component

---
+++ XUnitTestProject1/Controllers/After2510/Work2611TControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using EFCoreTesting.Controllers.After2510;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1.Controllers.After2510
+{
+    public class Work2611TControllerTests
+    {
+
+        [Fact]
+        public void ReturnComponentTest()
+        {
+            Work2611Controller controller = new Work2611Controller();
+            var res = controller.ReturnComponent();
+
+            var d = Assert.IsType<ViewComponentResult>(res);
+            Assert.Equal("{ Name = Из компонента вызов в виде возвращаемого значения метода }", d.Arguments.ToString());
+        }
+    }
+}
