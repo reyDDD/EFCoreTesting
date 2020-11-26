087e8d033edc10208286250581cad58aeb8f61b9
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-26 20:57:55 +0200

    tag helper

---
+++ EFCoreTesting/Infrastructure/TagHelpers/TH2611TagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("gg" , Attributes = "namee")]
+    public class TH2611TagHelper : TagHelper
+    {
+        [HtmlAttributeName("namee")]
+        public string Namee { get; set; }
+        public override async void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content = await output.GetChildContentAsync();
+            output.Content.Append($" yet slowly {Namee}");
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "p";
+        }
+    }
+}
