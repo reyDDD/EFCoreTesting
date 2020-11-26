d23e86f68026e01cdb89c362630b5d60203b49e5
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-26 16:58:40 +0200

    <vc:probn Name="privet"></vc:probn>

---
+++ EFCoreTesting/Components/Probn/ProbniyViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Components.Component2611
+{
+    [ViewComponent(Name = "Probn")]
+    public class ProbniyViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string Name)
+        {
+            return View("Default", $"{Name} добавка");
+        }
+    }
+}
