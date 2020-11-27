e230b21935d07a33fc116a181d678ceccf01f9ba
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-27 21:55:44 +0200

    [ViewContext] [HtmlAttributeNotBound]

--- EFCoreTesting/Infrastructure/TagHelpers/TH2611TagHelper.cs
+++ EFCoreTesting/Infrastructure/TagHelpers/TH2611TagHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +9,15 @@ using System.Threading.Tasks;
 namespace EFCoreTesting.Infrastructure.TagHelpers
 {
     [HtmlTargetElement("gg", Attributes = "namee", ParentTag = "div", TagStructure = TagStructure.NormalOrSelfClosing)]
-    [HtmlTargetElement("gg" , Attributes = "namee")]
+    [HtmlTargetElement("gg", Attributes = "namee")]
     public class TH2611TagHelper : TagHelper
     {
         [HtmlAttributeName("namee")]
         public string Namee { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContextData { get; set; }
         public override async void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.Content = await output.GetChildContentAsync();
@@ -19,7 +25,7 @@ namespace EFCoreTesting.Infrastructure.TagHelpers
             output.TagMode = TagMode.StartTagAndEndTag;
             output.TagName = "p";
             output.PreElement.SetHtmlContent("<i>ii</i>");
-            output.PostContent.SetHtmlContent(" <u>end</u>");
+            output.PostContent.SetHtmlContent($" <u>end: {ViewContextData.ViewData.Model.ToString()}</u>");
         }
     }
 }
