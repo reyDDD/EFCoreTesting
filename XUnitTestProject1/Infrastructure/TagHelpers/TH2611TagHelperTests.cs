af8293f37dd57d08ef6977db577f035d82eac0bb
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-27 21:06:45 +0200

    TH2611TagHelperTests

---
+++ XUnitTestProject1/Infrastructure/TagHelpers/TH2611TagHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using EFCoreTesting.Infrastructure.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1.Infrastructure.TagHelpers
+{
+    public class TH2611TagHelperTests
+    {
+        [Fact]
+        public void TagTest()
+        {
+            var context = new TagHelperContext(new TagHelperAttributeList(), new Dictionary<object, object>(), "uniqueId");
+            var output = new TagHelperOutput("p", new TagHelperAttributeList(), (cache, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+            TH2611TagHelper tH2611TagHelper = new TH2611TagHelper() { Namee = "forme" };
+            tH2611TagHelper.Process(context, output);
+
+            Assert.Equal(" yet slowly forme", output.Content.GetContent());
+        }
+    }
+}
