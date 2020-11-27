54d916337b2be6a7bb8ffce78031a5389ade3ade
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-27 21:50:07 +0200

    сужение

--- EFCoreTesting/Infrastructure/TagHelpers/TH2611TagHelper.cs
+++ EFCoreTesting/Infrastructure/TagHelpers/TH2611TagHelper.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace EFCoreTesting.Infrastructure.TagHelpers
 {
+    [HtmlTargetElement("gg", Attributes = "namee", ParentTag = "div", TagStructure = TagStructure.NormalOrSelfClosing)]
     [HtmlTargetElement("gg" , Attributes = "namee")]
     public class TH2611TagHelper : TagHelper
     {
@@ -17,6 +18,8 @@ namespace EFCoreTesting.Infrastructure.TagHelpers
             output.Content.Append($" yet slowly {Namee}");
             output.TagMode = TagMode.StartTagAndEndTag;
             output.TagName = "p";
+            output.PreElement.SetHtmlContent("<i>ii</i>");
+            output.PostContent.SetHtmlContent(" <u>end</u>");
         }
     }
 }
