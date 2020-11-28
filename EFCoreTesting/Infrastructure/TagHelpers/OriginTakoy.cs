fa6897b97198c2c03f0e306defef8a325c903261
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-28 16:03:01 +0200

    output.GetChildContentAsync()

---
+++ EFCoreTesting/Infrastructure/TagHelpers/OriginTakoy.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("factt", Attributes = "maye-buty")]
+    public class OriginTakoy : TagHelper
+    {
+        public override async void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content = await output.GetChildContentAsync();
+            output.Content.Append(" добавка");
+            output.TagName = "p";
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
