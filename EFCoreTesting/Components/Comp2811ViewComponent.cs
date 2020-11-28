5b6c1bdd103a6785dbd1343eedd6d6d1ba45929f
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-28 15:42:47 +0200

    Com2811

---
+++ EFCoreTesting/Components/Comp2811ViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Components
+{
+    [ViewComponent(Name = "Com2811")]
+    public class Comp2811ViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("compPereimenovaniy" as object);
+        }
+    }
+}
