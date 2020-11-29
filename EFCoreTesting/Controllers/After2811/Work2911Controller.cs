fb7baa710701a68ae42242ab4fe55a76db18dc3d
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-29 13:47:25 +0200

    new new()

---
+++ EFCoreTesting/Controllers/After2811/Work2911Controller.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2811
+{
+    public class Work2911Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new() { FirstName = "FirstName" };
+            var res1 = GetUser(new());
+            return View("Index", $"{user.FirstName} {res1.LastName}");
+        }
+
+        public User GetUser(User user)
+        {
+            //....work
+            return new() {LastName = "LastName" };
+        }
+    }
+}
