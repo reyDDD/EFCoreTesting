43f10771de20cde26a30e3ba4acf302a68e4c566
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-29 18:31:13 +0200

    типы записей C# 9.0

--- EFCoreTesting/Controllers/After2811/Work2911Controller.cs
+++ EFCoreTesting/Controllers/After2811/Work2911Controller.cs
@@ -3,12 +3,99 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EFCoreTesting.Controllers.After2811
 {
+    public record Person
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+
+        public Person(string first, string last) => (FirstName, LastName) = (first, last);
+    }
+
+    public record Person2
+    {
+        public string FromProperty { get; }
+
+        public Person2(string first) => FromProperty = first;
+
+        //переопределение базового метода PrintMembers для управления способом вывода элементов на печать
+        protected virtual bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"FromProperty такой проперти = {FromProperty}");
+            return true;
+        }
+    }
+
+    public sealed record Person4
+    {
+        public string FromProperty { get; }
+
+        public Person4(string first) => FromProperty = first;
+
+        //переопределение базового метода PrintMembers для управления способом вывода элементов на печать
+        private bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"FromProperty такой проперти = {FromProperty}");
+            return true;
+        }
+    }
+
+    public abstract record Person5
+    {
+        public string FromProperty { get; }
+
+        public Person5(string first) => FromProperty = first;
+
+        //переопределение базового метода PrintMembers для управления способом вывода элементов на печать
+        protected virtual bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"FromProperty такой проперти = {FromProperty}");
+            return true;
+        }
+    }
+
+    public record Person3(string FirstName, string LastName); // упрощенный вариант записи
+
     public class Work2911Controller : Controller
     {
+
+        public IActionResult Work3()
+        {
+            Person3 person = new Person3("personas", "fantomas");
+            return View("Index", person.ToString()); //переопределнный ToString возвращает Person3 { FirstName = personas, LastName = fantomas }
+        }
+
+        public IActionResult Work4()
+        {
+            Person3 person = new Person3("personas", "fantomas");
+            var (one, two) = person;//деконструкция записи в свойства
+            return View("Index", one);
+        }
+
+        public IActionResult Work5()
+        {
+            Person3 person = new Person3("personas", "fantomas");
+            Person3 brather = person with { FirstName = "bratelnik" }; //создание копии с измененным свойством
+            return View("Index", brather.FirstName);
+        }
+
+        public IActionResult Work6()
+        {
+            Person3 person = new Person3("personas", "fantomas");
+            Person3 brather = person with { }; //создание точной копии записи (значения не копируются, поверхностно копируются ссылки на значения)
+            return View("Index", brather.FirstName);
+        }
+
+        public IActionResult Work7()
+        {
+            Person2 person = new Person2("personas");
+            return View("Index", person.ToString());
+        }
+
         public IActionResult Work1()
         {
             User user = new User();
@@ -36,7 +123,7 @@ namespace EFCoreTesting.Controllers.After2811
         public User GetUser(User user)
         {
             //....work
-            return new() {LastName = "LastName" };
+            return new() { LastName = "LastName" };
         }
     }
 }
