c8a04599699c77f637cfd8d9d0737f9eaf8364ed
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-29 14:14:04 +0200

    шаблоны соответствия

--- EFCoreTesting/Controllers/After2811/Work2911Controller.cs
+++ EFCoreTesting/Controllers/After2811/Work2911Controller.cs
@@ -9,6 +9,16 @@ namespace EFCoreTesting.Controllers.After2811
 {
     public class Work2911Controller : Controller
     {
+        public IActionResult Work1()
+        {
+            User user = new User();
+            bool tr = user is User; //шаблон проверки соответсвия типу
+            bool tr2 = (tr is true and (4 > 3)) || (tr is not false); //шаблон в круглых скобках подчеркивает приоритет 
+            bool tr3 = (user.Id is (not 5) or (6 and > 2));
+            string tpe = tr2 == true ? "user is User" : "user is not User";
+            return View("Index", $"{tpe}");
+        }
+
         public IActionResult Index()
         {
             User user = new() { FirstName = "FirstName" };
