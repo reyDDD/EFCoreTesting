d4022866766b547c1f6af233cb83e86012ea4751
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-30 16:16:54 +0200

    local function

---
+++ EFCoreTesting/Controllers/After2811/Work3011Controller.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreTesting.Models.After3011;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Controllers.After2811
+{
+    public class Work3011Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            Model3011 model = new Model3011();
+            try
+            {
+                IEnumerable<string> res = model.GetList();
+                return View(res);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ EFCoreTesting/Models/After3011/Model3011.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTesting.Models.After3011
+{
+    public class Model3011
+    {
+        public IEnumerable<string> GetList()
+        {
+            IEnumerable<string> res;
+            try
+            {
+                res = LocalFunction();
+            }
+            catch(Exception ex)
+            {
+                res = new List<string>();
+                throw new Exception("работает");
+            }
+            return res;
+
+            IEnumerable<string> LocalFunction()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (i == 4)
+                    {
+                        throw new Exception("а не тут то было");
+                    }
+                    yield return $"{i}";
+                }
+            }
+        }
+    }
+}
